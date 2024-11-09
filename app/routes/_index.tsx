0e736dfaac31a97f07787b2ad478fcd19a9bb666
meshalTitum97 <meshal@titumlk.onmicrosoft.com>
2024-11-09 17:30:21 +0530

    project initialized

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -12,7 +12,7 @@ export default function Index() {
     <div className="flex h-screen items-center justify-center">
       <div className="flex flex-col items-center gap-16">
         <header className="flex flex-col items-center gap-9">
-          <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
+          <h1 className="leading motion-preset-float text-2xl font-bold text-gray-800 dark:text-gray-100">
             Welcome to <span className="sr-only">Remix</span>
           </h1>
           <div className="h-[144px] w-[434px]">
@@ -24,7 +24,7 @@ export default function Index() {
             <img
               src="/logo-dark.png"
               alt="Remix"
-              className="hidden w-full dark:block"
+              className="hidden  w-full dark:block"
             />
           </div>
         </header>
