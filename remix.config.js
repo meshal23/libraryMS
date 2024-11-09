f87e65bb22f4272a349f81f3ae414fdb9eed76a2
meshalTitum97 <meshal@titumlk.onmicrosoft.com>
2024-11-09 17:55:11 +0530

    flat routes installed

--- remix.config.js
+++ remix.config.js
@@ -1,5 +1,10 @@
+import { flatRoutes } from "remix-flat-routes";
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
+  ignoredRouteFiles: ["**/.*"],
+  routes: async (defineRoutes) => {
+    return flatRoutes("routes", defineRoutes);
+  },
   tailwind: true,
   postcss: true,
 };
